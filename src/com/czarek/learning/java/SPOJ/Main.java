aaf44a8189b8865d66144835c940cd34ab26ae8c
czaro79 <drocez@gmail.com>
2021-07-04 17:22:19 +0200

    2 commit - class Pupil modified - HashMap, toString

--- src/com/czarek/learning/java/SPOJ/Main.java
+++ src/com/czarek/learning/java/SPOJ/Main.java
@@ -5,20 +5,8 @@ import models.Pupil;
 public class Main {
 
     public static void main(String[] args) {
-        int[][] tableOfLogs = new int[][]{
-                {2, 3, 4, 5, 3},
-                {5, 4, 5, 6, 5},
-                {4, 5, 4, 4, 5},
-                {2, 3, 2, 4, 5},
-                {3, 4, 5, 5, 5},
-                {3, 4, 2, 2, 5}};
-
-        int[][] tab = new int[6][4];
-
-        Pupil pupil = new Pupil("Czarek Drobiazg", tableOfLogs);
-
+        Pupil pupil = new Pupil("Cezary","Drobiazg");
         System.out.println(pupil);
 
-        System.out.println(pupil.getPupilName());
     }
 }
--- src/models/Pupil.java
+++ src/models/Pupil.java
@@ -1,47 +1,75 @@
 package models;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Pupil {
-    private String pupilName;
-    private final int[][] pupilGradingLog;
+    private String pupilFirstName;
+    private String pupilLastName;
+    HashMap<Subject, ArrayList<Double>> pupilMarks;
 
-    public Pupil(String pupilName, int[][] logs) {
-        if (pupilName == null || pupilName.isBlank()) {
-            throw new IllegalArgumentException("Pupil must have a name! - cannot be null/blank)");
+    public Pupil(String pupilFirstName, String pupilLastName) {
+        if (pupilFirstName == null || pupilFirstName.isBlank()) {
+            throw new IllegalArgumentException("Pupil must have a first name! - cannot be null/blank)");
         }
-        this.pupilName = pupilName;
-        this.pupilGradingLog = Arrays.copyOf(logs, logs.length);
+        if (pupilLastName == null || pupilLastName.isBlank()) {
+            throw new IllegalArgumentException("Pupil must have a last name! - cannot be null/blank)");
+        }
+        this.pupilFirstName = pupilFirstName;
+        this.pupilLastName = pupilLastName;
+        this.pupilMarks = new HashMap<Subject, ArrayList<Double>>();
+
+        this.pupilMarks.put(Subject.ENGLISH, new ArrayList<Double>());
+        this.pupilMarks.put(Subject.BIOLOGY,new ArrayList<Double>());
+        this.pupilMarks.put(Subject.MATHEMATICS, new ArrayList<Double>());
+        this.pupilMarks.put(Subject.FRENCH, new ArrayList<Double>());
+        this.pupilMarks.put(Subject.PHYSICS, new ArrayList<Double>());
+        this.pupilMarks.put(Subject.CHEMISTRY, new ArrayList<Double>());
     }
 
     public Pupil(Pupil source) {
-        this.pupilName = source.pupilName;
-        this.pupilGradingLog = Arrays.copyOf(source.pupilGradingLog, source.pupilGradingLog.length);
+        this.pupilFirstName = source.pupilFirstName;
+        this.pupilLastName = source.pupilLastName;
+        this.pupilMarks = (HashMap<Subject, ArrayList<Double>>) Map.copyOf(source.pupilMarks);
     }
 
-    public String getPupilName() {
-        return pupilName;
+    public String getPupilFirstName() {
+        return pupilFirstName;
     }
 
-    public void setPupilName(String pupilName) {
-        if (pupilName == null || pupilName.isBlank()) {
-            throw new IllegalArgumentException("Pupil must have a name! - cannot be null/blank)");
+    public String getPupilLastName() {
+        return pupilLastName;
+    }
+
+    public void setPupilFirstName(String pupilFirstName) {
+        if (pupilFirstName == null || pupilFirstName.isBlank()) {
+            throw new IllegalArgumentException("Pupil must have a first name! - cannot be null/blank)");
         }
-        this.pupilName = pupilName;
+        this.pupilFirstName = pupilFirstName;
+    }
+
+    public void setPupilLastName(String pupilLastName) {
+        if (pupilLastName == null || pupilLastName.isBlank()) {
+            throw new IllegalArgumentException("Pupil must have a last name! - cannot be null/blank)");
+        }
+        this.pupilLastName = pupilLastName;
     }
 
     public String toString() {
-        StringBuilder temp = new StringBuilder("Name: " + pupilName + "\n");
-
-        for (int[] ints : pupilGradingLog) {
-            for (int anInt : ints) {
-                temp.append(anInt).append(" ");
-            }
-            temp.append("\n");
+        String temp = "Pupil: " + getPupilFirstName() + " " + getPupilLastName() + "\n";
+        int firstLineLength = temp.length();
+        for (int i = 0; i < firstLineLength; i++) {
+            temp += "-";
         }
-
-        temp.append("\n");
-
-        return temp.toString();
+        temp += "\n";
+        temp += Subject.ENGLISH + ": " + this.pupilMarks.get(Subject.ENGLISH).toString() + "\n" +
+                Subject.BIOLOGY + ": " + this.pupilMarks.get(Subject.BIOLOGY).toString() + "\n" +
+                Subject.MATHEMATICS + ": " + this.pupilMarks.get(Subject.MATHEMATICS).toString() + "\n" +
+                Subject.FRENCH + ": " + this.pupilMarks.get(Subject.FRENCH).toString() + "\n" +
+                Subject.PHYSICS + ": " + this.pupilMarks.get(Subject.PHYSICS).toString() + "\n" +
+                Subject.CHEMISTRY + ": " + this.pupilMarks.get(Subject.CHEMISTRY).toString() + "\n\n";
+        return temp;
     }
 }
